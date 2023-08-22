a1473b245520a8befd425a5992c200cf10a1462a
LyDoan0302 <138420217+LyDoan0302@users.noreply.github.com>
2023-08-22 15:44:30 +0700

    up_bai_tap_ss16

---
+++ project/src/ss16_IO_text_file/practice/CharArrayInput.java
@@ -0,0 +1,2 @@
+package ss16_IO_text_file.practice;public class TryQuiz {
+}
---
+++ project/src/ss16_IO_text_file/practice/IO_stream_Demo.java
@@ -0,0 +1,54 @@
+package ss16_IO_text_file.practice;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WriterDemo {
+    public static void writeFile(String pathName, List<String> data) {
+        File file = null;
+        FileWriter fileWriter = null;
+        BufferedWriter bufferedWriter = null;
+
+        try {
+            file = new File(pathName);
+            fileWriter = new FileWriter(file);
+            bufferedWriter = new BufferedWriter(fileWriter);
+
+            for(String d: data) {
+                bufferedWriter.write(d);
+                bufferedWriter.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if(bufferedWriter != null) {
+                    bufferedWriter.close();
+                }
+                if(fileWriter != null) {
+                    fileWriter.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();}
+        }
+    }
+
+    public static List<String> readFile(String pathName) {
+        File file = null;
+        FileReader fileReader = null;
+        BufferedReader bufferedReader = null;
+        List<String> result = new ArrayList<>();
+
+        try {
+            file = new File(pathName);
+            fileReader = new FileReader(file);
+            bufferedReader = new BufferedReader(fileReader);
+
+            String str;
+            while ((str = bufferedReader.readLine()) != null )
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
